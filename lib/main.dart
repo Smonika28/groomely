70e53541b97066d55f27bed60c46a8c095dcf233
pandey211998@gmail.com <pandey211998@gmail.com>
2023-07-06 22:20:51 +0530

    bug fixex with duration time

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:groomely_seller/feature/booking/bloc/booking_history_bloc.dart';
-import 'package:groomely_seller/feature/booking/widgets/booking_history_invocie.dart';
 import 'package:groomely_seller/feature/dashboard_screen/bloc/home_view_bloc.dart';
 import 'package:groomely_seller/feature/login/bloc/seller_login_bloc.dart';
+import 'package:groomely_seller/feature/login/presentation/login_screen.dart';
 import 'package:groomely_seller/feature/profile/bloc/user_profile_bloc.dart';
 import 'package:groomely_seller/feature/service/add_service_screen/bloc/fetch_all_service_bloc.dart';
 import 'package:groomely_seller/feature/service/manage_service/bloc/manage_service_bloc.dart';
 import 'package:groomely_seller/routes/app_routes.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+   await Future.delayed(const Duration(milliseconds: 300));
   
   runApp(MyApp());
 }
@@ -31,31 +31,18 @@ class MyApp extends StatelessWidget {
         BlocProvider<FetchAllFieldBloc>(create: (context)=>FetchAllFieldBloc())
       ],
       child: MaterialApp(
-        
         theme: ThemeData(
           visualDensity: VisualDensity.standard,
           useMaterial3: true
         ),
         title: 'grooumelly',
-        
         debugShowCheckedModeBanner: false,
         initialRoute: AppRoutes.loginScreen,
         routes: AppRoutes.routes,
-        // home: MyTest(),
+        // home: LoginScreen(),
       ),
     );
   }
 }
-class MyTest extends StatelessWidget {
-  const MyTest({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("test"),
-      ),
-      body: Center(child: Text("Body Test")),
-    );
-  }
-}
+
